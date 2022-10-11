68ceca98da97fcaec3fedf642306422e753b3156
ramazankaran <ramazankaran50@gmail.com>
2022-10-11 06:11:40 +0300

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private double vize;
+        private double final;
+        private double ortalama;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Label1.Text = "";
+            Label2.Text = "";
+            label3.Text = "";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                {
+                    {
+                        vize = Convert.ToDouble
+                    }
+                }
+            }
+        }
+    }
+
+}
